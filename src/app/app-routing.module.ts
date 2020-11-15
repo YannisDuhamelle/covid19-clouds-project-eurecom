f2870b6452a6637dcaea39bffa0dd9aaf9c38eef
yannis.duhamelle <yannis.duhamelle@etu.imt-lille-douai.fr>
2020-11-15 21:11:46 +0100

    Adding the route to the worldwide summary

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { WorldwideSummaryComponent } from './worldwide-summary/worldwide-summary.component';
+
+const routes: Routes = [
+  { path: "", pathMatch: "full", component: WorldwideSummaryComponent },
+  { path: "**", redirectTo: "signin" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
