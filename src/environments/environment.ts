d173919b31210dfef5dff6857c05cf41a2df6ff2
yannis.duhamelle <yannis.duhamelle@etu.imt-lille-douai.fr>
2020-11-15 18:37:42 +0100

    Link to the firebase project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDre0Buh4s-tD_Lg4jHDJOAby9e2-uFaE0",
+    authDomain: "covid19-ff0fd.firebaseapp.com",
+    databaseURL: "https://covid19-ff0fd.firebaseio.com",
+    projectId: "covid19-ff0fd",
+    storageBucket: "covid19-ff0fd.appspot.com",
+    messagingSenderId: "706973362500",
+    appId: "1:706973362500:web:cf74f5b5f831c2ba70cf11",
+    measurementId: "G-4NZJH34PR6"
+  }
 };
 
 /*
