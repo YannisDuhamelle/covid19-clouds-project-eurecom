274bea9d4f85f58e55165261537ee46862c81db4
yannis.duhamelle <yannis.duhamelle@etu.imt-lille-douai.fr>
2020-11-17 10:24:50 +0100

    Creation of the bar chart of the last 7 days

--- src/app/worldwide-data.service.ts
+++ src/app/worldwide-data.service.ts
@@ -12,7 +12,29 @@ export class WorldwideDataService {
   constructor(private http: HttpClient) { }
 
   /** GET data from the API */
-  getDataFromAPI(){
+  getDataFromAPIWorldSummary(){
     return this.http.get(this.apiCovid19SummaryUrl);
+  }
+  getDataFromAPIWorldPerDay(day: Date, dayPlusOne: Date) {
+    let valueOfDay: number = day.getDate();
+    let valueOfDayPlusOne: number = dayPlusOne.getDate();
+    let apiCovid19WorldPerDayUrl = 'https://api.covid19api.com/world';
+    console.log("Voici le mois du jour : " + day.getMonth());
+    apiCovid19WorldPerDayUrl += "?from=" + day.getFullYear() + "-" + (day.getMonth()+1) + "-";
+    if (valueOfDay < 10) {
+      apiCovid19WorldPerDayUrl += "0" + day.getDate();
+    }
+    else {
+      apiCovid19WorldPerDayUrl += day.getDate();
+    }
+    apiCovid19WorldPerDayUrl += "T00:00:00Z&to=" + dayPlusOne.getFullYear() + "-" + (dayPlusOne.getMonth()+1) + "-";
+    if (valueOfDayPlusOne < 10) {
+      apiCovid19WorldPerDayUrl += "0" + dayPlusOne.getDate() + "T00:00:00Z";
+    }
+    else {
+      apiCovid19WorldPerDayUrl += dayPlusOne.getDate() + "T00:00:00Z";
+    }
+    console.log(apiCovid19WorldPerDayUrl);
+    return this.http.get(apiCovid19WorldPerDayUrl);
   }
 }
--- src/app/worldwide-summary/worldwide-summary.component.ts
+++ src/app/worldwide-summary/worldwide-summary.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WorldwideDataService } from '../worldwide-data.service';
-import { ChartType, ChartOptions } from 'chart.js';
+import { ChartDataSets, ChartType, ChartOptions } from 'chart.js';
 import { Label } from 'ng2-charts';
 
 @Component({
@@ -11,6 +11,7 @@ import { Label } from 'ng2-charts';
 export class WorldwideSummaryComponent implements OnInit {
 
   dataFromAPI: any;
+  dataFromAPIWorldPerDay: any;
 
   public pieChartOptions: ChartOptions = {responsive: true,legend: {position: 'top'}};
   public pieChartLabels: Label[] = ['Dead Cases', 'Recovered Cases', 'Active Cases'];
@@ -23,18 +24,66 @@ export class WorldwideSummaryComponent implements OnInit {
     },
   ];
 
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: { xAxes: [{}], yAxes: [{}] },
+  };
+  public barChartLabels: Label[] = [];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData: ChartDataSets[] = [];
+
   constructor(private dataService: WorldwideDataService) { }
 
   ngOnInit() {
-    this.dataService.getDataFromAPI().subscribe(data => {
+    this.dataService.getDataFromAPIWorldSummary().subscribe(data => {
       this.dataFromAPI = data;
-      this.generateCharts();
+      this.generatePieCharts();
+    });
+    
+    let today = new Date();
+    let dayMinus7 = new Date(today);
+    dayMinus7.setDate(dayMinus7.getDate() - 7);
+    this.dataService.getDataFromAPIWorldPerDay(dayMinus7, today).subscribe(dataReceive => {
+      const data: { [index: string]: any } = dataReceive;
+      console.log(data);
+      console.log(data[0]);
+      console.log(data[0]["NewDeaths"]);
+      let dailyDeath: number[] = [];
+      let dailyRecovered: number[] = [];
+      let dailyNewCase: number[] = [];
+      for (let i = data.length - 1; i >= 0; i--) {
+        dailyDeath.push(data[i]["NewDeaths"]);
+        dailyRecovered.push(data[i]["NewRecovered"]);
+        dailyNewCase.push(data[i]["NewConfirmed"]);
+        console.log("Yannis");
+        //console.log(dailyDeath);
+      }
+      console.log("Fait!")
+      console.log(dailyDeath);
+      this.generateBarCharts(dailyDeath,dailyRecovered,dailyNewCase);
     });
   }
 
-  generateCharts() {
+  generatePieCharts() {
     let activeCases = this.dataFromAPI.Global["TotalConfirmed"] - (this.dataFromAPI.Global["TotalRecovered"] + this.dataFromAPI.Global["TotalDeaths"])
     this.pieChartData = [this.dataFromAPI.Global["TotalDeaths"], this.dataFromAPI.Global["TotalRecovered"], activeCases];
   }
-
+  generateBarCharts(dailyDeath: number[], dailyRecovered: any[], dailyNewCase: any[]) {
+    console.log("Cretion of the chart");
+    this.barChartData = [
+      { data: dailyDeath, label: 'Daily Deaths' },
+      { data: dailyRecovered, label: 'Daily Recovered' },
+      { data: dailyNewCase, label: 'Daily New Cases' }
+    ];
+    let today = new Date();
+    let day = new Date(today);
+    for (let i = dailyDeath.length-1; i >= 0; i--) {
+      let day = new Date(today);
+      day.setDate(day.getDate() - i);
+      this.barChartLabels.push(day.toDateString())
+    }
+    console.log(this.barChartData);
+  }
 }
