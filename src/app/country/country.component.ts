e4e19384020e9c27dab7938f50604957163cc712
yannis.duhamelle <yannis.duhamelle@eurecom.fr>
2020-11-25 16:34:52 +0100

    Reading the URL to display the name of the country

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-country',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryComponent implements OnInit {
 
-  constructor() { }
+  name: String | undefined;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    let url = this.router.url;
+    let brut_name = url.split('/')[2];
+    while (brut_name.search("%20") != -1) {
+      brut_name = brut_name.replace("%20", " ");
+    }
+    this.name = brut_name;
   }
 
 }
