cfe51a9e9e1d4fe08a6670246e0e364770929892
yannis.duhamelle <yannis.duhamelle@eurecom.fr>
2020-12-27 00:12:37 +0100

    Front de la page add-news

--- src/app/add-news/add-news.component.ts
+++ src/app/add-news/add-news.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewsComponent implements OnInit {
 
-  constructor() { }
+  apiCovid19CountriesUrl = "https://api.covid19api.com/summary";
+  countries: { [index: string]: any; } | undefined;
+
+  constructor(private http: HttpClient) {
+    this.getDataFromAPIWorldCountries().subscribe((doc) => {
+      console.log(doc);
+      const countries_raw: { [index: string]: any } = doc;
+      this.countries = countries_raw.Countries;
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  getDataFromAPIWorldCountries() {
+    return this.http.get(this.apiCovid19CountriesUrl);
   }
 
 }
